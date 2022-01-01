f9f270868b0c8f17bd2b0b31ff0dd6c3c8e5b78c
Firli Riki Efendi <84231731+firliriki@users.noreply.github.com>
2022-01-01 23:36:36 +0700

    update verifikasi

--- resources/views/auth/reg.blade.php
+++ resources/views/auth/reg.blade.php
@@ -9,11 +9,23 @@
                 <div class="card-body">
                     <form method="POST" action="{{ url('/regs') }}">
                         @csrf
-                        <div class="col-md-12">
-                            <p>kami akan mengirimkan sms kode verifikasi ke no 0{{Auth::user()->NoHP}} pastikan nomor dalam keadaan aktif</p>
-                        </div>   
+                        @csrf
                        
-                            <div class="col-md-12 offset-md-4">
+                        <div class="form-group row">
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
+                            <div class="col-md-6">
+                                <input id="phone_number" type="tel" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="+{{ Auth::user()->NoHP }}" required readonly>
+                                @error('phone_number')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                       
+                      
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
